91571ec87f5fb1fdbc866f7bacdc09f47726a09a
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2023-01-19 15:58:35 -0300

    feat: :sparkles: Adding fontFamily Inter

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold
+} from '@expo-google-fonts/inter'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
+
+
+  if(!fontsLoaded){
+    return
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text style={{fontFamily: "Inter_400Regular", fontSize: 16}}>Teste de fonte Inter Regular</Text>
+      <Text style={{fontFamily: "Inter_500Medium", fontSize: 16}}>Teste de fonte Inter Medium</Text>
+      <Text style={{fontFamily: "Inter_600SemiBold", fontSize: 16}}>Teste de fonte Inter Semibold</Text>
+      <Text style={{fontFamily: "Inter_700Bold", fontSize: 16}}>Teste de fonte Inter Bold</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
